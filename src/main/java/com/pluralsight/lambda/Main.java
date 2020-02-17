be203fd6dc92524b0fcd3e538b989be9c679a018
Jeff Wong <v-jwong1@expedia.com>
2020-02-17 17:50:58 +1000

    create Person class

--- src/main/java/com/pluralsight/lambda/Main.java
+++ src/main/java/com/pluralsight/lambda/Main.java
@@ -3,25 +3,28 @@ package com.pluralsight.lambda;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.concurrent.Executors;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        String[] strings = new String[5];
-        strings[0] = "ap";
-        strings[1] = "b";
-        strings[2] = "candy";
-        strings[3] = "dilator";
-        strings[4] = "ear";
-
-        Comparator<String> comparator1 = (s1, s2) -> Integer.compare(s1.length(), s2.length());
-
-        Arrays.sort(strings, comparator1);
-
-        System.out.println(strings[0]);
-        System.out.println(strings[1]);
-        System.out.println(strings[2]);
-        System.out.println(strings[3]);
-        System.out.println(strings[4]);
+//        String[] strings = new String[5];
+//        strings[0] = "ap";
+//        strings[1] = "b";
+//        strings[2] = "candy";
+//        strings[3] = "dilator";
+//        strings[4] = "ear";
+//
+//        Comparator<String> comparator1 = (s1, s2) -> Integer.compare(s1.length(), s2.length());
+//
+//        Arrays.sort(strings, comparator1);
+//
+//        System.out.println(strings[0]);
+//        System.out.println(strings[1]);
+//        System.out.println(strings[2]);
+//        System.out.println(strings[3]);
+//        System.out.println(strings[4]);
 
 //        Executors.newSingleThreadExecutor().execute(() -> {
 //            for (int i = 0; i < 10; i++) {
