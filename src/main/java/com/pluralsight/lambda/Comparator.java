2d5039af4c95182c4192ad696f87eec76ee4f176
Jeff Wong <v-jwong1@expedia.com>
2020-02-17 20:28:25 +1000

    create thenComparing default non-static method for instances of Comparator interface

--- src/main/java/com/pluralsight/lambda/Comparator.java
+++ src/main/java/com/pluralsight/lambda/Comparator.java
@@ -6,9 +6,11 @@ import java.util.function.Function;
 public interface Comparator<T> {
     int compare(T t1, T t2);
 
+    default Comparator<T> thenComparing(Comparator<T> comparator) {
+        return (p1, p2) -> compare(p1, p2) == 0 ? comparator.compare(p1, p2) : compare(p1, p2);
+    };
+
     static <U> Comparator<U> comparing(Function<U, Comparable> f) {
         return (p1, p2) -> f.apply(p1).compareTo(f.apply(p2));
-    }
-
-
+    };
 }
