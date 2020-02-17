396d20f6aab2af80403de22ea18b6bbb9f2a637b
Jeff Wong <v-jwong1@expedia.com>
2020-02-17 13:47:33 +1000

    create anonymous class of Runnable and use Executors to run it on single thread

--- src/main/java/com/pluralsight/lambda/Main.java
+++ src/main/java/com/pluralsight/lambda/Main.java
@@ -2,29 +2,41 @@ package com.pluralsight.lambda;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        String[] strings = new String[5];
-        strings[0] = "ap";
-        strings[1] = "b";
-        strings[2] = "candy";
-        strings[3] = "dilator";
-        strings[4] = "ear";
+//        String[] strings = new String[5];
+//        strings[0] = "ap";
+//        strings[1] = "b";
+//        strings[2] = "candy";
+//        strings[3] = "dilator";
+//        strings[4] = "ear";
+//
+//        Comparator<String> comparator1 = new Comparator<String>() {
+//            @Override
+//            public int compare(String s1, String s2) {
+//                return Integer.compare(s1.length(), s2.length());
+//            }
+//        };
+//
+//        Arrays.sort(strings, comparator1);
+//
+//        System.out.println(strings[0]);
+//        System.out.println(strings[1]);
+//        System.out.println(strings[2]);
+//        System.out.println(strings[3]);
+//        System.out.println(strings[4]);
 
-        Comparator<String> comparator1 = new Comparator<String>() {
+        final Runnable runnable1 = new Runnable() {
             @Override
-            public int compare(String s1, String s2) {
-                return Integer.compare(s1.length(), s2.length());
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("It works! - " + i);
+                }
             }
         };
 
-        Arrays.sort(strings, comparator1);
-
-        System.out.println(strings[0]);
-        System.out.println(strings[1]);
-        System.out.println(strings[2]);
-        System.out.println(strings[3]);
-        System.out.println(strings[4]);
+        Executors.newSingleThreadExecutor().execute(runnable1);
     }
 }
