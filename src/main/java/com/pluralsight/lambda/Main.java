38a1df58af1fb6eb13390b9fcb754f238532bfc5
Jeff Wong <v-jwong1@expedia.com>
2020-02-18 17:04:50 +1000

    construct an example of Consumer interface using lambda expression

--- src/main/java/com/pluralsight/lambda/Main.java
+++ src/main/java/com/pluralsight/lambda/Main.java
@@ -2,19 +2,14 @@ package com.pluralsight.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        Predicate<String> predicate1 = s -> s.length() == 3;
-
-        System.out.println(predicate1.test("kkk"));
-        System.out.println(predicate1.test("kkka"));
-        System.out.println(predicate1.test("kk"));
-        System.out.println(predicate1.test("k"));
-        System.out.println(predicate1.test("kkk"));
-
+//        Predicate<String> predicate1 = s -> s.length() == 3;
+//
 //        List<String> list1 = new ArrayList<>();
 //        list1.add("ear");
 //        list1.add("jim");
@@ -22,6 +17,9 @@ public class Main {
 //
 //        List<String> list2 = list1.stream().filter(predicate1).collect(Collectors.toList());
 //        System.out.println(list2);
+
+        Consumer<String> consumer1 = System.out::println;
+        consumer1.accept("essasdf");
     }
 
     private static void useComparator() {
