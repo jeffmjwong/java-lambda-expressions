4b4b239209144117281944d229c77774aa051add
Jeff Wong <v-jwong1@expedia.com>
2020-02-23 16:46:58 +1000

    complete City class

--- src/main/java/com/pluralsight/lambda/City.java
+++ src/main/java/com/pluralsight/lambda/City.java
@@ -1,4 +1,19 @@
 package com.pluralsight.lambda;
 
 public class City {
+    private String name;
+
+    public City() {};
+    public City(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "City{name=" + name + "}";
+    }
 }
