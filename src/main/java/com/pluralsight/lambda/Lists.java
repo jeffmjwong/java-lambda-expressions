e7c5f4be62b5b916ae90d70b26983a9b77a008cd
Jeff Wong <v-jwong1@expedia.com>
2020-02-24 15:57:08 +1000

    implemented filter method for custom Lists class

--- src/main/java/com/pluralsight/lambda/Lists.java
+++ src/main/java/com/pluralsight/lambda/Lists.java
@@ -3,10 +3,15 @@ package com.pluralsight.lambda;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Lists {
     public static <T, U> List<U> map(List<T> list, Function<T, U> function) {
         return list.stream().map(function).collect(Collectors.toList());
+    }
+
+    public static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
+        return list.stream().filter(predicate).collect(Collectors.toList());
     }
 }
--- src/main/java/com/pluralsight/lambda/Main.java
+++ src/main/java/com/pluralsight/lambda/Main.java
@@ -19,7 +19,8 @@ public class Main {
 
         List<Person> people = new ArrayList<>(Arrays.asList(p1, p2, p3));
         List<Integer> ages = Lists.map(people, Person::getAge);
-//        List<Integer> agesGreaterThan20 = Lists.filter(ages, age -> age > 20);
+        List<Integer> agesGreaterThan20 = Lists.filter(ages, age -> age > 30);
+        System.out.println(agesGreaterThan20);
 //        int sum = Lists.reduce(agesGreaterThan20, (a1, a2) -> a1 + a2);
     }
 
