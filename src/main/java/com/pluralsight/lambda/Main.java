49dfc1de40aab6fa8094d45739effedd83fb173c
Jeff Wong <v-jwong1@expedia.com>
2020-02-24 20:51:09 +1000

    use Integer::max instead of sum for reduction lambda

--- src/main/java/com/pluralsight/lambda/Main.java
+++ src/main/java/com/pluralsight/lambda/Main.java
@@ -20,7 +20,7 @@ public class Main {
         List<Person> people = new ArrayList<>(Arrays.asList(p1, p2, p3));
         List<Integer> ages = Lists.map(people, Person::getAge);
         List<Integer> agesGreaterThan20 = Lists.filter(ages, age -> age > 30);
-        int sum = Lists.reduce(agesGreaterThan20, Integer::sum);
+        int sum = Lists.reduce(agesGreaterThan20, Integer::max);
         System.out.println(sum);
     }
 
