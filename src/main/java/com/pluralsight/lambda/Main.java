ddd809c446e38d6089c9a9eb78a87180c5589d1d
Jeff Wong <v-jwong1@expedia.com>
2020-02-25 17:36:15 +1000

    remove Lists factory class as it is not the ideal way to implement map/filter/reduce patterns on collections

--- src/main/java/com/pluralsight/lambda/Main.java
+++ src/main/java/com/pluralsight/lambda/Main.java
@@ -23,27 +23,6 @@ public class Main {
 //        List<Integer> agesGreaterThan20 = Lists.filter(ages, age -> age > 50);
 //        int sum = Lists.reduce(agesGreaterThan20, Integer::max);
 //        System.out.println(sum);
-
-        List<Integer> ints = Arrays.asList(0,1,2,3,4,5,6,7,8,9);
-        List<Integer> ints1 = Arrays.asList(0,1,2,3,4);
-        List<Integer> ints2 = Arrays.asList(5,6,7,8,9);
-        BinaryOperator<Integer> op = (i1, i2) -> (i1 + i2)/2;
-
-        int reduction = reduce(ints, 0, op);
-
-//        int reduction1 = reduce(ints1, 0, op);
-//        int reduction2 = reduce(ints2, 0, op);
-//        int reduction = reduce(Arrays.asList(reduction1, reduction2), 0, op);
-
-        System.out.println("Reduction: " + reduction);
-    }
-
-    private static int reduce(List<Integer> values, int valueIfEmpty, BinaryOperator<Integer> reduction) {
-        int result = valueIfEmpty;
-        for (int value : values) {
-            result = reduction.apply(result, value);
-        }
-        return result;
     }
 
     private static void useNewMethodsOfCollections() {
